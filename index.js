c645b478703ba5bb091e1c56ae05733de06e18d3
Fanny Simon <contact.fanny.simon@gmail.com>
2025-03-04 13:15:05 +0100

    Css hover, ajout de classList aux todos

--- index.js
+++ index.js
@@ -13,25 +13,44 @@
 // --------------------------------------------------------------------------------------------------------------
 
 const form = document.querySelector("form");
-let todo = "";
-const li = document.querySelector("li");
+const inputText = document.querySelector('input[type="text"]');
+let inputTodo = "";
+let newLi = document.querySelector(".items > ul");
+const todoListContainer = document.querySelector(".todolist-container");
+// let size = 30; // Taille initiale
 
 // récupère le texte qui est tapé en temps réel dans l'input:
-input.addEventListener("input", (e) => {
-  todo = e.target.value;
-  console.log(todo);
+inputText.addEventListener("input", (e) => {
+  inputTodo = e.target.value;
 });
 
 // Ajouter un todo à la liste au submit
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  document.querySelector(".items > ul").innerHTML += `
-  <li>${todo}</li>`;
+  newLi.innerHTML += `
+  <li>${inputTodo}</li>`;
+  input.value = ""; // Vider l'input
+  // size += 5;
+  // todoListContainer.style.height = size + "%";
 });
 
-// Supprimer un todo quand on click dessus: (.remove()) => bubbles)
-li.addEventListener("click", () => {
-  console.log("test");
-  li.remove();
+// Changement de l'icône et du texte en after au survole et au clik du todo :
+newLi.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.classList.add("beforeChecked");
+    e.target.classList.add("afterSuppr");
+    // if (e.target.classList.contains("beforeChecked")) {  // Vérifie si l'élément possède la classe "to-remove"
+    //   e.target.remove();  // Supprime l'élément du DOM
+    // }
+  }
 });
+
+// Explications:
+// Que fait e.target.tagName === "LI" ?
+// Dans l'événement click, e.target représente l'élément exact sur lequel tu as cliqué.
+
+// Avec e.target.tagName, on récupère le nom de la balise de cet élément (en majuscules).
+
+// Donc, if (e.target.tagName === "LI") signifie :
+// 👉 "Si l'élément cliqué est une <li>, alors fais quelque chose" (ici, le supprimer).
